2a61d2b161eac8cf6a68717044f4fc5662ef7152
Rennar <rennarsrepair@gmail.com>
2022-08-04 10:44:43 -0500

    fix for module18

---
+++ .history/Module18/main_20220804104433.js
@@ -0,0 +1,20 @@
+const orig = {
+    test: 'test',
+    userDetails: {
+        address: {
+            province: 'Province',
+            country: 'country'
+        },
+        name: 'myObject'
+    },
+    myFn: () => {},
+    undef: undefined,
+}
+
+const copy = JSON.parse(JSON.stringify(orig));
+
+copy.userDetails.name = 'newName'
+
+console.log('orig:', orig);
+
+console.log('copy:', copy);
--- Module18/main.js
+++ Module18/main.js
@@ -1,10 +1,20 @@
-const orig = ['avocado', 'apple'];  
+const orig = {
+    test: 'test',
+    userDetails: {
+        address: {
+            province: 'Province',
+            country: 'country'
+        },
+        name: 'myObject'
+    },
+    myFn: () => {},
+    undef: undefined,
+}
 
-const copy = orig;
+const copy = JSON.parse(JSON.stringify(orig));
 
-copy.push('guava');
+copy.userDetails.name = 'newName'
 
 console.log('orig:', orig);
 
 console.log('copy:', copy);
-
